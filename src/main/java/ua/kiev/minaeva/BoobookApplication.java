141c104ceb7738e107fefd3ed266482cf6788eef
denys.burda <denys.burda@intellectsoft.net>
2020-05-21 12:02:44 +0300

    init commit

---
+++ src/main/java/ua/kiev/minaeva/BoobookApplication.java
@@ -0,0 +1,13 @@
+package ua.kiev.minaeva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/kiev/minaeva/BoobookApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kiev.minaeva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoobookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
