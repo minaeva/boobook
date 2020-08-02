c0efe075ed158a65c6e57a95b59da8222811d22e
minaeva <sminaeva@uscold.com>
2020-08-02 11:31:58 +0300

    not nedeed methods removed

--- src/main/java/ua/kiev/minaeva/service/BookService.java
+++ src/main/java/ua/kiev/minaeva/service/BookService.java
@@ -16,18 +16,6 @@ public interface BookService {
 
     BookDto setActive(Long bookId) throws BoobookNotFoundException;
 
-    BookDto setInactive(Long bookId) throws BoobookNotFoundException;
-
-    BookDto setActive(Long bookId) throws BoobookNotFoundException;
-
-    BookDto setInactive(Long bookId) throws BoobookNotFoundException;
-
-    BookDto setActive(Long bookId) throws BoobookNotFoundException;
-
-    BookDto setInactive(Long bookId) throws BoobookNotFoundException;
-
-    BookDto setActive(Long bookId) throws BoobookNotFoundException;
-
     void deleteBook(BookDto bookDto);
 
     List<BookDto> getAll();
